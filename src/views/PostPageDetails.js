b7115d29d89cdc8b7a081cb5c16f46e33bcbd71a
Rong Sheng <soh.cayden@gmail.com>
2025-01-15 23:53:55 +0800

    'preprod13'

--- src/views/PostPageDetails.js
+++ src/views/PostPageDetails.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 export default function PostPageDetails() {
   const { bookId } = useParams();

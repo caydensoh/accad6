ced031de5e08b18a97c2a195b4784367ecccb167
Soh Rong Sheng <soh.cayden@gmail.com>
2025-01-16 11:48:35 +0800

    'v1.01'

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,8 @@
-# Step 1: Use an official Node.js image to build the app
 FROM node:18 AS build
-
-# Step 2: Set the working directory inside the container
 WORKDIR /app
-
-# Step 3: Copy the package.json and package-lock.json
 COPY package.json package-lock.json ./
-
-# Step 4: reinstall dependencies just in case
 RUN npm install
-
-# Step 5: Copy the rest of the app
 COPY . .
-
-# Step 6: Build the app
 RUN npm run build
 EXPOSE 8080
 CMD ["npm","start"]
--- src/App.test.js
+++ src/App.test.js
@@ -4,40 +4,16 @@ import App from './App';
 import PostPageUpdate from './views/PostPageUpdate'; 
 import PostPageDetails from './views/PostPageDetails'; 
 import PostPageAdd from './views/PostPageAdd'; 
-import HomePage from './views/HomePage'; 
+import PostPageHome from './views/PostPageHome'; 
  
 test('True', () => { 
     expect(true).toBe(true); 
 }); 
  
-test('renders learn react link', () => { 
+test('home page exists', () => { 
   render(<App />); 
-  const linkElement = screen.getByText(/learn react/i); 
+  const linkElement = screen.getByText(/book list/i); 
   expect(linkElement).toBeInTheDocument(); 
-}); 
- 
-test('routes to update page', () => { 
-  render( 
-    <MemoryRouter initialEntries={['/update/1']}> 
-      <Routes> 
-        <Route path="/update/:id" element={<PostPageUpdate />} /> 
-      </Routes> 
-    </MemoryRouter> 
-  ); 
-  const updateElement = screen.getByText(/update/i); 
-  expect(updateElement).toBeInTheDocument(); 
-}); 
- 
-test('routes to details page', () => { 
-  render( 
-    <MemoryRouter initialEntries={['/details/1']}> 
-      <Routes> 
-        <Route path="/details/:id" element={<PostPageDetails />} /> 
-      </Routes> 
-    </MemoryRouter> 
-  ); 
-  const detailsElement = screen.getByText(/details/i); 
-  expect(detailsElement).toBeInTheDocument(); 
 }); 
  
 test('routes to add page', () => { 
@@ -48,18 +24,6 @@ test('routes to add page', () => {
       </Routes> 
     </MemoryRouter> 
   ); 
-  const addElement = screen.getByText(/add/i); 
+  const addElement = screen.getByText(/add book/i); 
   expect(addElement).toBeInTheDocument(); 
 }); 
- 
-test('routes to home page', () => { 
-  render( 
-    <MemoryRouter initialEntries={['/']}> 
-      <Routes> 
-        <Route path="/" element={<HomePage />} /> 
-      </Routes> 
-    </MemoryRouter> 
-  ); 
-  const homeElement = screen.getByText(/home/i); 
-  expect(homeElement).toBeInTheDocument(); 
-});

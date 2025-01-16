52c4759945e5037832d04c1df6af309dd4c7d488
Soh Rong Sheng <soh.cayden@gmail.com>
2025-01-16 13:31:13 +0800

    'v1.0.2'

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import PostPageUpdate from './views/PostPageUpdate';
 import PostPageDetails from './views/PostPageDetails'; 
 import PostPageAdd from './views/PostPageAdd'; 
 import PostPageHome from './views/PostPageHome'; 
-import { render, screen } from '@testing-library/react'; 
+import { render } from 'react-dom'; 
 import { MemoryRouter, Route, Routes } from 'react-router-dom'; 
 import App from './App'; 
  
